7c18a5fd1ce5ffcc7887d4befa05ac536dc0a4cf
Thiago FSA <72234344+ThiagoFsa2006@users.noreply.github.com>
2026-09-11 19:45:24 -0300

    Change button font size from 28px to 24px

--- meu-projeto/app.py
+++ meu-projeto/app.py
@@ -80,7 +80,7 @@ div[data-testid="stButton"] button,
 div[data-testid="stButton"] button *,
 div[data-testid="stButton"] button span,
 div[data-testid="stButton"] button p{
-    font-size:28px !important;
+    font-size:24px !important;
     font-weight:700 !important;
     color:#244029 !important;
 }
