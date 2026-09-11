965e56643095842f20f109773892378d99fe6761
Thiago FSA <72234344+ThiagoFsa2006@users.noreply.github.com>
2026-09-11 20:11:57 -0300

    Update image rotation in app.py
    Removed 'expand=True' from the rotate method.

--- meu-projeto/app.py
+++ meu-projeto/app.py
@@ -166,7 +166,7 @@ if os.path.exists(banner_path):
     img = Image.open(banner_path)
 
     # gira a foto
-    img = img.rotate(270, expand=True)
+    img = img.rotate(270)
 
     # transforma em banner horizontal
     largura_desejada = 1600
