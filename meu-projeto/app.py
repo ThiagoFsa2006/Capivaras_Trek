46d47fd31747f9be5e84ff9e1cb4608ad6416fca
Thiago FSA <72234344+ThiagoFsa2006@users.noreply.github.com>
2026-09-11 20:16:11 -0300

    Update app.py

--- meu-projeto/app.py
+++ meu-projeto/app.py
@@ -167,7 +167,7 @@ if os.path.exists(banner_path):
     img = Image.open(banner_path)
 
     # gira para a esquerda
-    img = img.rotate(270, expand=True)
+    img = img.rotate(270)
 
     largura, altura = img.size
 
