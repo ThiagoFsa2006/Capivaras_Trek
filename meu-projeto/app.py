2013c878247cda99f2b2f1a9073c6db5b55510e6
Thiago FSA <72234344+ThiagoFsa2006@users.noreply.github.com>
2026-09-11 13:19:02 -0300

    Refactor app layout and hide Streamlit components
    Updated the Streamlit app to hide header and toolbar.

--- meu-projeto/app.py
+++ meu-projeto/app.py
@@ -1,23 +1,32 @@
 import streamlit as st
 
-st.set_page_config(page_title="Hello World", layout="wide")
-
-st.markdown(
-    """
-    <div style="
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 80vh;
-    ">
-        <h1 style="
-            color: black;
-            font-size: 36px;
-            margin: 0;
-        ">
-            Hello World
-        </h1>
-    </div>
-    """,
-    unsafe_allow_html=True
+st.set_page_config(
+    page_title="Hello World",
+    layout="wide"
 )
+
+st.markdown("""
+<style>
+[data-testid="stHeader"] {
+    display: none;
+}
+[data-testid="stToolbar"] {
+    display: none;
+}
+[data-testid="stDecoration"] {
+    display: none;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<div style="
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:100vh;">
+    <h1 style="color:black;font-size:36px;">
+        Hello World
+    </h1>
+</div>
+""", unsafe_allow_html=True)
