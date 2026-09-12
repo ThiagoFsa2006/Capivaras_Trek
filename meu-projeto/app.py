b2e23127d362e1f181418ca00153b9df33893aea
Thiago FSA <72234344+ThiagoFsa2006@users.noreply.github.com>
2026-09-11 21:24:41 -0300

    Update hero titles and slide duration in app.py

--- meu-projeto/app.py
+++ meu-projeto/app.py
@@ -290,21 +290,21 @@ def render_hero_carousel():
           <!-- SLIDE 1 -->
           <div class="swiper-slide slide-1" style="background-image: url('{img1}');">
             <div class="hero-title-container">
-              <h1 class="hero-title">CUME<br>DO<br>TAIPABUÇO</h1>
+              <h1 class="hero-title">CUME DO TAIPABUÇO</h1>
             </div>
           </div>
 
           <!-- SLIDE 2 -->
           <div class="swiper-slide slide-2" style="background-image: url('{img2}');">
             <div class="hero-title-container">
-              <h1 class="hero-title">CACHOEIRA<br>DA<br>LAPINHA</h1>
+              <h1 class="hero-title">CACHOEIRA DA LAPINHA</h1>
             </div>
           </div>
 
           <!-- SLIDE 3 -->
           <div class="swiper-slide slide-3" style="background-image: url('{img3}');">
             <div class="hero-title-container">
-              <h1 class="hero-title">CUME<br>DO<br>ANHANGAVA</h1>
+              <h1 class="hero-title">CUME DO ANHANGAVA</h1>
             </div>
           </div>
 
@@ -321,7 +321,7 @@ def render_hero_carousel():
         var swiper = new Swiper(".mySwiper", {{
           loop: true,
           autoplay: {{
-            delay: 7000, /* 7 segundos de duração por slide */
+            delay: 6000, /* 7 segundos de duração por slide */
             disableOnInteraction: false,
           }},
           pagination: {{
