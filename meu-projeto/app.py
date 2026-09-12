e291ed8f716eb6452c6d1830844cd032782e87ef
Thiago FSA <72234344+ThiagoFsa2006@users.noreply.github.com>
2026-09-11 21:15:25 -0300

    Adjust background positions for slide elements

--- meu-projeto/app.py
+++ meu-projeto/app.py
@@ -221,15 +221,15 @@ def render_hero_carousel():
         /* Regras customizadas de enquadramento para cada slide */
         .slide-1 {{
           /* Ajuste aqui a porcentagem vertical (0% = topo, 50% = centro, 100% = base) */
-          background-position: center 20%;
-        }}
-
-        .slide-2 {{
           background-position: center 40%;
         }}
 
-        .slide-3 {{
+        .slide-2 {{
           background-position: center 50%;
+        }}
+
+        .slide-3 {{
+          background-position: center 40%;
         }}
 
         .swiper-slide::before {{
