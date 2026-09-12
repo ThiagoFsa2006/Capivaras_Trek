05cfadcf5b695e81fd9cc1b1e49164a58be5328b
Thiago FSA <72234344+ThiagoFsa2006@users.noreply.github.com>
2026-09-11 21:17:13 -0300

    Update app.py

--- meu-projeto/app.py
+++ meu-projeto/app.py
@@ -225,11 +225,11 @@ def render_hero_carousel():
         }}
 
         .slide-2 {{
-          background-position: center 30%;
+          background-position: center 35%;
         }}
 
         .slide-3 {{
-          background-position: center 60%;
+          background-position: center 55%;
         }}
 
         .swiper-slide::before {{
